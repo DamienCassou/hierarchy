0a819c3e0fb02cb038f83f2afd02676ac8ea111c
Damien Cassou <damien@cassou.me>
2017-04-13 07:22:21 +0200

    Remove hierarchy-examples-fs.el from the package
    * This is not needed by users.

--- hierarchy-examples-fs.el
+++ hierarchy-examples-fs.el
@@ -3,8 +3,6 @@
 ;; Copyright (C) 2017  Damien Cassou
 
 ;; Author: Damien Cassou <damien@cassou.me>
-;; Version: 0.1.0
-;; Package-Requires: ((emacs "25.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
